17e8c7a48de5d04a9a4d8f920c43abff2c03da14
Dan Neag <54548058+dneag@users.noreply.github.com>
2022-12-04 19:20:20 -0500

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/game-review/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/game-review/" : "/game-review/",
 };
